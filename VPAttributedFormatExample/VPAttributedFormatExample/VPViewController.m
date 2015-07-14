c9bfa16701e8dd6864a4f5892310909e77b1d6cc
Vladimir Popko <vvv.popko@gmail.com>
2015-07-13 23:31:28 -0500

    Updated labels properties names

--- VPAttributedFormatExample/VPAttributedFormatExample/VPViewController.m
+++ VPAttributedFormatExample/VPAttributedFormatExample/VPViewController.m
@@ -16,33 +16,33 @@
 @property (nonatomic, weak) IBOutlet UIView *basicFormatsView;
 @property (nonatomic, weak) IBOutlet NSLayoutConstraint *basicFormatsViewTrailingSpace;
 
-@property (nonatomic, weak) IBOutlet UILabel *basicLabelFormat1;
-@property (nonatomic, weak) IBOutlet UILabel *basicLabelValue1;
+@property (nonatomic, weak) IBOutlet UILabel *basicFormatLabel1;
+@property (nonatomic, weak) IBOutlet UILabel *basicValueLabel1;
 
-@property (nonatomic, weak) IBOutlet UILabel *basicLabelFormat2;
-@property (nonatomic, weak) IBOutlet UILabel *basicLabelValue2;
+@property (nonatomic, weak) IBOutlet UILabel *basicFormatLabel2;
+@property (nonatomic, weak) IBOutlet UILabel *basicValueLabel2;
 
-@property (nonatomic, weak) IBOutlet UILabel *basicLabelFormat3;
-@property (nonatomic, weak) IBOutlet UILabel *basicLabelValue3;
+@property (nonatomic, weak) IBOutlet UILabel *basicFormatLabel3;
+@property (nonatomic, weak) IBOutlet UILabel *basicValueLabel3;
 
-@property (nonatomic, weak) IBOutlet UILabel *basicLabelFormat4;
-@property (nonatomic, weak) IBOutlet UILabel *basicLabelValue4;
+@property (nonatomic, weak) IBOutlet UILabel *basicFormatLabel4;
+@property (nonatomic, weak) IBOutlet UILabel *basicValueLabel4;
 
 // Pro.
 @property (nonatomic, weak) IBOutlet UIView *proFormatsView;
 @property (nonatomic, weak) IBOutlet NSLayoutConstraint *proFormatsViewLeadingSpace;
 
-@property (nonatomic, weak) IBOutlet UILabel *proLabelFormat1;
-@property (nonatomic, weak) IBOutlet UILabel *proLabelValue1;
+@property (nonatomic, weak) IBOutlet UILabel *proFormatLabel1;
+@property (nonatomic, weak) IBOutlet UILabel *proValueLabel1;
 
-@property (nonatomic, weak) IBOutlet UILabel *proLabelFormat2;
-@property (nonatomic, weak) IBOutlet UILabel *proLabelValue2;
+@property (nonatomic, weak) IBOutlet UILabel *proFormatLabel2;
+@property (nonatomic, weak) IBOutlet UILabel *proValueLabel2;
 
-@property (nonatomic, weak) IBOutlet UILabel *proLabelFormat3;
-@property (nonatomic, weak) IBOutlet UILabel *proLabelValue3;
+@property (nonatomic, weak) IBOutlet UILabel *proFormatLabel3;
+@property (nonatomic, weak) IBOutlet UILabel *proValueLabel3;
 
-@property (nonatomic, weak) IBOutlet UILabel *proLabelFormat4;
-@property (nonatomic, weak) IBOutlet UILabel *proLabelValue4;
+@property (nonatomic, weak) IBOutlet UILabel *proFormatLabel4;
+@property (nonatomic, weak) IBOutlet UILabel *proValueLabel4;
 
 @end
 
@@ -54,17 +54,17 @@
     // Basic.
     [self.view bringSubviewToFront:self.basicFormatsView];
     self.basicFormatsViewTrailingSpace.constant = 0;
-    [self fillBasicLabelValue1];
-    [self fillBasicLabelValue2];
-    [self fillBasicLabelValue3];
-    [self fillBasicLabelValue4];
+    [self fillBasicValueLabel1];
+    [self fillBasicValueLabel2];
+    [self fillBasicValueLabel3];
+    [self fillBasicValueLabel4];
     
     // Pro.
     self.proFormatsViewLeadingSpace.constant = 0;
-    [self fillProLabelValue1];
-    [self fillProLabelValue2];
-    [self fillProLabelValue3];
-    [self fillProLabelValue4];
+    [self fillProValueLabel1];
+    [self fillProValueLabel2];
+    [self fillProValueLabel3];
+    [self fillProValueLabel4];
 }
 
 - (IBAction)onSegmentControlValueChanged:(UISegmentedControl *)sender {
@@ -78,25 +78,25 @@
 #pragma mark -
 #pragma mark Basic
 
-- (void)fillBasicLabelValue1 {
+- (void)fillBasicValueLabel1 {
     NSString *hot = @"Hot";
     NSString *cold = @"Cold";
     
-    self.basicLabelValue1.attributedText = [NSAttributedString attributedStringWithAttributedFormat:self.basicLabelFormat1.attributedText,
+    self.basicValueLabel1.attributedText = [NSAttributedString attributedStringWithAttributedFormat:self.basicFormatLabel1.attributedText,
                                             hot,
                                             cold];
 }
 
-- (void)fillBasicLabelValue2 {
+- (void)fillBasicValueLabel2 {
     int mile = 1;
     double kilometer = 1.61;
     
-    self.basicLabelValue2.attributedText = [NSAttributedString attributedStringWithAttributedFormat:self.basicLabelFormat2.attributedText,
+    self.basicValueLabel2.attributedText = [NSAttributedString attributedStringWithAttributedFormat:self.basicFormatLabel2.attributedText,
                                             mile,
                                             kilometer];
 }
 
-- (void)fillBasicLabelValue3 {
+- (void)fillBasicValueLabel3 {
     NSInteger hour = 0;
     NSInteger minute = 0;
     NSInteger second = 0;
@@ -104,7 +104,7 @@
     [calendar getHour:&hour minute:&minute second:&second nanosecond:NULL fromDate:[NSDate date]];
     int width = 2;
     
-    self.basicLabelValue3.attributedText = [NSAttributedString attributedStringWithAttributedFormat:self.basicLabelFormat3.attributedText,
+    self.basicValueLabel3.attributedText = [NSAttributedString attributedStringWithAttributedFormat:self.basicFormatLabel3.attributedText,
                                             width,
                                             (long)hour,
                                             width,
@@ -113,12 +113,12 @@
                                             (long)second];
 }
 
-- (void)fillBasicLabelValue4 {
+- (void)fillBasicValueLabel4 {
     int number = 50;
     int percent = 20;
     int result = number * percent / 100;
     
-    self.basicLabelValue4.attributedText = [NSAttributedString attributedStringWithAttributedFormat:self.basicLabelFormat4.attributedText,
+    self.basicValueLabel4.attributedText = [NSAttributedString attributedStringWithAttributedFormat:self.basicFormatLabel4.attributedText,
                                             number,
                                             percent,
                                             result];
@@ -127,19 +127,19 @@
 #pragma mark -
 #pragma mark Pro
 
-- (void)fillProLabelValue1 {
+- (void)fillProValueLabel1 {
     
 }
 
-- (void)fillProLabelValue2 {
+- (void)fillProValueLabel2 {
     
 }
 
-- (void)fillProLabelValue3 {
+- (void)fillProValueLabel3 {
 
 }
 
-- (void)fillProLabelValue4 {
+- (void)fillProValueLabel4 {
     
 }
 
